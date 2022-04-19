c738b4c6d9dada52785238e0f56bfe938a3dc40d
snowy <snowstormleopard5@gmail.com>
2022-04-20 05:42:53 +0800

    updated

--- src/components/about-section/index.js
+++ src/components/about-section/index.js
@@ -11,10 +11,10 @@ const AboutSection = () => {
                 <h2 className="text-uppercase pb-3 about-sec-title">About</h2>
             </div>
             <div className="p-4 about-sec-content">
-                <p>Owning a DAG Node outright is out of reach for many of us. What would be the next best thing? Being part owner amongst your peers.
+                <p>Owning a DAG node outright is out of reach for many of us. What would be the next best thing? Being part owner amongst your peers.
                 </p>
                 <p>How ser? How can I own part of a DAG node?</p>
-                <p>Well my fren, the answer to that is 5,500 Zaidan Clan Samurai NFTs! The majority of the proceeds will be used to purchase Dag nodes and other tokens of the ecosystem. We will reinvest pre-mainnet rewards to accumulate more nodes.</p>
+                <p>Well my fren, the answer to that is 5,500 Zaidan Clan Samurai NFTs! The majority of the proceeds will be used to purchase DAG nodes and other tokens of the ecosystem. We will reinvest pre-mainnet rewards to accumulate more nodes.</p>
                 <p>NFT holders will be eligible for receiving revenue generated from DAG nodes (post mainnet) & other investments. Read our tokenomics for better understanding.
                 </p>
             </div>
